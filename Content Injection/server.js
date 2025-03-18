5d1111be4a7772223176e048cccdcd3afa727b08
Nobin Johnson <nobinjohnson@Nobins-Mac-mini.lan>
2025-03-17 21:52:09 -0400

    started chat app

---
+++ Content Injection/server.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+app.use(express.static('public'))
+const expressApp=app.listen(4000);
+const socketio=require('socket.io')
+const io=socketio(expressApp,{
+
+})
+
+io.on('connect',socket=>{console.log(socket.id," has joined our server!")})
